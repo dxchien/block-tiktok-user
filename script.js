f142944fcffe000f9be95ea24a8072a441ceb5f5
Đào Xuân Chiến <dxchien@gmail.com>
2025-09-29 17:03:12 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Auto block user by content
 // @namespace    http://tampermonkey.net/
-// @version      1.5
+// @version      1.6
 // @description  try to take over the world!
 // @author       dxchien
 // @match        https://www.tiktok.com/*
